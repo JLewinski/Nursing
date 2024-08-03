b237d86110220de8ba5ba1207fc5e03664a7f185
Jacob Lewinski <j1lewinski@gmail.com>
2024-08-03 15:34:54 -0500

    Init history page

--- Nursing.Mobile/Services/IDatabase.cs
+++ Nursing.Mobile/Services/IDatabase.cs
@@ -1,14 +1,13 @@
 ﻿using Nursing.Models;
 
-namespace Nursing.Mobile.Services
+namespace Nursing.Mobile.Services;
+
+internal interface IDatabase
 {
-    internal interface IDatabase
-    {
-        Task Delete(Guid id);
-        Task<(TimeSpan averageTotal, TimeSpan averageRight, TimeSpan averageLeft)> GetAverages(DateTime? start, DateTime? end);
-        Task<List<Feeding>> GetFeedings(DateTime? start, DateTime? end);
-        Task<List<Feeding>> GetLast();
-        Task<bool> SaveFeeding(Feeding feeding);
-        Task<TimeSpan> GetInBetween();
-    }
+    Task Delete(Guid id);
+    Task<(TimeSpan averageTotal, TimeSpan averageRight, TimeSpan averageLeft)> GetAverages(DateTime? start, DateTime? end);
+    Task<List<Feeding>> GetFeedings(DateTime? start, DateTime? end);
+    Task<List<Feeding>> GetLast();
+    Task<bool> SaveFeeding(Feeding feeding);
+    Task<TimeSpan> GetInBetween();
 }
