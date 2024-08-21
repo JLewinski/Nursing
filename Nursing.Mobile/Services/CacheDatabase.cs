b4ef3b0cf9436bcdacae29403250571ec1b80301
Jacob Lewinski <j1lewinski@gmail.com>
2024-08-21 16:16:53 -0500

    Using json to store data for now

---
+++ Nursing.Mobile/Services/CacheDatabase.cs
@@ -0,0 +1,97 @@
+﻿using Nursing.Models;
+using System.Text.Json;
+
+namespace Nursing.Mobile.Services;
+
+internal class CacheDatabase : IDatabase
+{
+    private const string DatabaseFilename = "Nursing.json";
+    private static string DatabasePath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DatabaseFilename);
+
+    public CacheDatabase()
+    {
+        //File.Delete(DatabasePath);
+    }
+
+    private async Task Init()
+    {
+        if (File.Exists(DatabasePath))
+        {
+            return;
+        }
+
+        await File.WriteAllTextAsync(DatabasePath, JsonSerializer.Serialize(new List<Feeding>()));
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var all = await GetFeedings();
+        var feeding = all.First(x => x.Id == id);
+        all.Remove(feeding);
+        await Save(all);
+    }
+
+    private async Task Save(List<Feeding> feedings)
+    {
+        var json = JsonSerializer.Serialize(feedings);
+        await File.WriteAllTextAsync(DatabasePath, json);
+    }
+
+    public async Task<(TimeSpan averageTotal, TimeSpan averageRight, TimeSpan averageLeft)> GetAverages(DateTime? start, DateTime? end)
+    {
+        var data = await GetFeedings(start, end);
+        var (total, right, left) = data.Aggregate((total: TimeSpan.Zero, right: TimeSpan.Zero, left: TimeSpan.Zero), (acc, x) =>
+        {
+            acc.total += x.TotalTime;
+            acc.right += x.RightBreastTotal;
+            acc.left += x.LeftBreastTotal;
+            return acc;
+        });
+
+        return (total / data.Count, right / data.Count, left / data.Count);
+    }
+
+    public async Task<List<Feeding>> GetFeedings(DateTime? start = null, DateTime? end = null)
+    {
+        await Init();
+
+        var jsonData = await File.ReadAllTextAsync(DatabasePath);
+
+        if (jsonData == string.Empty)
+        {
+            return [];
+        }
+
+        var feedings = JsonSerializer.Deserialize<List<Feeding>>(jsonData);
+        return feedings?
+            .Where(x => x.LastFinish >= (start ?? DateTime.MinValue) && x.LastFinish <= (end ?? DateTime.UtcNow))
+            .OrderByDescending(x => x.LastFinish)
+            .ToList() ?? [];
+    }
+
+    public async Task<List<Feeding>> GetLast()
+    {
+        var all = await GetFeedings();
+        return all.Take(2).ToList();
+    }
+
+    public async Task<bool> SaveFeeding(Feeding feeding)
+    {
+        var all = await GetFeedings();
+        var existing = all.FirstOrDefault(x => x.Id == feeding.Id);
+        
+        if (existing is not null)
+        {
+            all.Remove(existing);
+        }
+        else
+        {
+            feeding.Id = Guid.NewGuid();
+        }
+
+        all.Add(feeding);
+        await Save(all);
+
+        return true;
+    }
+}
---
+++ Nursing.Mobile/Services/LocalDatabase.cs
@@ -0,0 +1,104 @@
+﻿using Nursing.Models;
+using SQLite;
+
+namespace Nursing.Mobile.Services;
+
+internal class LocalDatabase : IDatabase
+{
+    private const string DatabaseFilename = "Nursing.db3";
+
+    public const SQLite.SQLiteOpenFlags Flags =
+        // open the database in read/write mode
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        // create the database if it doesn't exist
+        SQLite.SQLiteOpenFlags.Create |
+        // enable multi-threaded database access
+        SQLite.SQLiteOpenFlags.SharedCache;
+
+    private static string DatabasePath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DatabaseFilename);
+
+    private SQLiteAsyncConnection Database = null!;
+
+    private async Task Init()
+    {
+        if (Database is not null)
+        {
+            return;
+        }
+
+        File.Delete(DatabasePath);
+
+        Database = new SQLiteAsyncConnection(DatabasePath, Flags);
+
+        await Database.CreateTableAsync<Feeding>();
+    }
+
+    public async Task<bool> SaveFeeding(Feeding feeding)
+    {
+        await Init();
+        int result;
+        if (feeding.Id == Guid.Empty)
+        {
+            feeding.Id = Guid.NewGuid();
+            result = await Database.InsertAsync(feeding);
+        }
+        else
+        {
+            result = await Database.UpdateAsync(feeding);
+        }
+
+        return result == 1;
+    }
+
+    public async Task Delete(Guid id)
+    {
+        await Init();
+        await Database.Table<Feeding>().DeleteAsync(x => x.Id == id);
+    }
+
+    public async Task<List<Feeding>> GetFeedings(DateTime? start, DateTime? end)
+    {
+        await Init();
+        return await Database.Table<Feeding>()
+            .Where(x => x.LastFinish >= (start ?? DateTime.MinValue) && x.LastFinish <= (end ?? DateTime.UtcNow))
+            .OrderByDescending(x => x.LastFinish)
+            .ToListAsync();
+    }
+
+    public async Task<(TimeSpan averageTotal, TimeSpan averageRight, TimeSpan averageLeft)> GetAverages(DateTime? start, DateTime? end)
+    {
+        await Init();
+        var data = await Database.Table<Feeding>()
+            .Where(x => x.LastFinish >= (start ?? DateTime.MinValue) && x.LastFinish <= (end ?? DateTime.UtcNow))
+            .ToListAsync();
+
+        var (total, right, left) = data.Aggregate((total: TimeSpan.Zero, right: TimeSpan.Zero, left: TimeSpan.Zero), (acc, x) =>
+        {
+            acc.total += x.RightBreastTotal + x.LeftBreastTotal;
+            acc.right += x.RightBreastTotal;
+            acc.left += x.LeftBreastTotal;
+            return acc;
+        });
+
+        return (
+            TimeSpan.FromTicks(total.Ticks / data.Count),
+            TimeSpan.FromTicks(right.Ticks / data.Count),
+            TimeSpan.FromTicks(left.Ticks / data.Count));
+    }
+
+    public async Task<List<Feeding>> GetLast()
+    {
+        try
+        {
+            await Init();
+            return await Database.Table<Feeding>()
+            .OrderByDescending(x => x.LastFinish)
+            .Take(2)
+            .ToListAsync();
+        }
+        catch (Exception)
+        {
+            return new List<Feeding>();
+        }
+    }
+}
