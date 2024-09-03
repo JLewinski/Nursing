7093eb75a2bd5b3219ae31827d9c1e7a526a469d
Jacob Lewinski <j1lewinski@gmail.com>
2024-09-02 23:06:07 -0500

    Fix SQLite context setup

--- Nursing.Mobile/MauiProgram.cs
+++ Nursing.Mobile/MauiProgram.cs
@@ -24,7 +24,7 @@ public static class MauiProgram
         builder.Services.AddSingleton<HttpClient>();
 
         var sqliteFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Nursing.db");
-        builder.Services.AddDbContext<IDatabase, EFDatabase>(options =>
+        builder.Services.AddDbContext<EFDatabase>(options =>
             options.UseSqlite($"Data Source={sqliteFilePath}", opts =>
                 opts.MigrationsAssembly("Nursing.Sqlite")));
 
