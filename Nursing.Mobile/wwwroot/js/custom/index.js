37549b0a3c2634f839efa66d86842533ab63f796
Jacob Lewinski <j1lewinski@gmail.com>
2024-09-05 12:58:41 -0500

    Update theme manager and add loading popup

--- Nursing.Mobile/wwwroot/js/custom/index.js
+++ Nursing.Mobile/wwwroot/js/custom/index.js
@@ -1,14 +1,48 @@
+///<reference path="../../../node_modules/@types/bootstrap/index.d.ts" />
 //@ts-check
 
-/**
- * 
- * @param {boolean} isDarkTheme
- */
-function toggleTheme(isDarkTheme) {
-    const htmlTag = document.getElementsByTagName('html')[0];
-    htmlTag.setAttribute('data-bs-theme', isDarkTheme ? 'dark' : 'light');
+var themeManager = {
+    /**
+     * 
+     * @param {boolean} isDarkTheme
+     */
+    toggleTheme(isDarkTheme) {
+        const htmlTag = document.getElementsByTagName('html')[0];
+        htmlTag.setAttribute('data-bs-theme', isDarkTheme ? 'dark' : 'light');
+    },
+
+    isDarkPreferred() {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches;
+    }
 };
 
-function isDarkPreferred() {
-    return window.matchMedia('(prefers-color-scheme: dark)').matches;
-}
+var loadingModal = (() => {
+
+    class LoadingModal {
+        /** @type {bootstrap.Modal} */
+        #loadingModal;
+
+        init() {
+            if (this.#loadingModal) {
+                return;
+            }
+            this.#loadingModal = new bootstrap.Modal(document.getElementById('loadingModal'));
+        }
+
+        /**
+         * 
+         * @param {string} message
+         */
+        showLoadingModal(message) {
+            this.init();
+            document.getElementById('LoadingTitle').textContent = message;
+            this.#loadingModal.show();
+        }
+
+        hideLoadingModal() {
+            this.#loadingModal?.hide();
+        }
+    }
+
+    return new LoadingModal()
+})();
