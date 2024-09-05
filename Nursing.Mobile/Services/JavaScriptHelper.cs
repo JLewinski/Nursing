9bc98b24ed4a3e2f4a2a50b4ba905c82b2abb914
Jacob Lewinski <j1lewinski@gmail.com>
2024-09-05 13:10:35 -0500

    Make some extensions for JSInterop

---
+++ Nursing.Mobile/Services/JavaScriptHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+
+namespace Nursing.Mobile.Services;
+
+public static class JavaScriptHelper
+{
+    public static async Task ToggleTheme(this IJSRuntime JSRuntime, bool isDarkTheme)
+    {
+        await JSRuntime.InvokeVoidAsync("themeManager.toggleTheme", isDarkTheme);
+    }
+
+    public static async Task<bool> CheckIsDarkPrefered(this IJSRuntime JSRuntime)
+    {
+        return await JSRuntime.InvokeAsync<bool>("themeManager.isDarkPreferred");
+    }
+
+    public static async Task ShowLoadingModal(this IJSRuntime JSRuntime, string message = "Loading")
+    {
+        await JSRuntime.InvokeVoidAsync("loadingModal.showLoadingModal", message);
+    }
+
+    public static async Task HideLoadingModal(this IJSRuntime JSRuntime)
+    {
+        await JSRuntime.InvokeVoidAsync("loadingModal.hideLoadingModal");
+    }
+}
