2a6fa2148c587b512f904daec26700a222243ecf
Jacob Lewinski <j1lewinski@gmail.com>
2024-09-17 20:02:44 -0500

    Use blazored toasts

---
+++ Nursing.Blazor/ToastHelper.cs
@@ -0,0 +1,18 @@
+﻿using Blazored.Toast.Services;
+
+namespace Nursing.Blazor;
+
+public static class ToastHelper
+{
+    public static void ShowToast(this IToastService toastService, string message, bool success)
+    {
+        if (success)
+        {
+            toastService.ShowSuccess(message);
+        }
+        else
+        {
+            toastService.ShowError(message);
+        }
+    }
+}
