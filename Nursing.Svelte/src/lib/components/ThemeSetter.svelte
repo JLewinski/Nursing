b244424a411bd05d763b53a3c8de1a9029f4f0c3
Jacob Lewinski <jdlewinski@outlook.com>
2024-11-10 10:01:47 -0600

    Update ThemeSetter.svelte

--- Nursing.Svelte/src/lib/components/ThemeSetter.svelte
+++ Nursing.Svelte/src/lib/components/ThemeSetter.svelte
@@ -1,3 +1,13 @@
 <script lang="ts">
-    
+    import { settings } from '$lib/stores/settingsStore';
+    let selectedTheme = $derived.by(() => {
+        if (settings.theme === 'system') {
+            return 'dark'; //todo: check system theme
+        }
+        return system.theme;
+    });
 </script>
+
+<div data-theme="{selectedTheme">
+    {$props.children}
+</div>
