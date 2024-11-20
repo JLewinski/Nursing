00ae11c54739864fa2930bb1a5cacffff0992fb0
Jacob Lewinski <jdlewinski@outlook.com>
2024-11-20 16:11:20 -0600

    Ignoring Docker, static is working

--- Nursing.Svelte/svelte.config.js
+++ Nursing.Svelte/svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const config: import('@sveltejs/kit').Config = {
+/** @type {import('@sveltejs/kit').Config} */
+
+const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
@@ -9,7 +11,7 @@ const config: import('@sveltejs/kit').Config = {
     }),
     files: {
       serviceWorker: 'static/service-worker'
-    }
+    },
   }
 };
 
